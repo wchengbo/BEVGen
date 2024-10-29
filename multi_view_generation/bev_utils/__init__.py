78e0f8aaca79bf9982813b265017632f32a0e981
wchengbo <24210980106@m.fudan.edu.cn>
2024-10-29 18:42:02 +0800

    Update __init__.py

--- multi_view_generation/bev_utils/__init__.py
+++ multi_view_generation/bev_utils/__init__.py
@@ -5,5 +5,5 @@ from multi_view_generation.bev_utils.nuscenes_helper import CLASSES
 from multi_view_generation.bev_utils.visualize import camera_bev_grid, batched_camera_bev_grid, viz_bev, argoverse_camera_bev_grid, raw_output_data_bev_grid, save_binary_as_image, return_binary_as_image
 
 ARGOVERSE_DIR = Path(os.getenv('ARGOVERSE_DATA_DIR', 'datasets/av2')).expanduser().resolve()
-NUSCENES_DIR = Path(os.getenv('NUSCENES_DATA_DIR', 'datasets/nuscenes')).expanduser().resolve()
-SAVE_DATA_DIR = Path(os.getenv('SAVE_DATA_DIR', 'datasets')).expanduser().resolve()
+NUSCENES_DIR = Path(os.getenv('NUSCENES_DATA_DIR', 'datasets/nuscenes')).expanduser().resolve()#获取nuScenes 数据集的绝对路径
+SAVE_DATA_DIR = Path(os.getenv('SAVE_DATA_DIR', 'datasets')).expanduser().resolve()#保存数据的路径
